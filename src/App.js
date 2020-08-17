ecce583d6d830fadb88ccb6b8b2b3f066690d494
gabrielgeorgiev1998 <gabrielgeorgiev11@gmail.com>
2020-08-17 20:03:49 +0300

    nothing sure whats going on yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Slider } from '@material-ui/core';
+
+function randomArray(size) {
+  var array = [];
+  for(let i=0;i<size;i++) {
+    array.push(Math.floor(Math.random() * 200) + 10);
+  }
+
+  return array;
+}
 
 function App() {
+  const [size, setSize] = useState(50);
+  const [array, setArray] = useState(randomArray(size));
+
+  const handleSortPress = () => {
+    setSize(100);
+    setArray(randomArray(100));
+  }
+  const sliderOnChange = (event, value) => {
+    setSize(value);
+    setArray(randomArray(size));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen items-center">
+      <div className="flex flex-row w-full bg-red-800 items-center justify-around" style={{ height: '8%' }}>
+        <div className="w-1/4 pl-4">
+          <Slider
+            min={50}
+            max={250}
+            step={1}
+            value={size}
+            onChange={sliderOnChange}
+            aria-labelledby="continuous-slider"
+          />
+        </div>
+        <button onClick={handleSortPress}>
+          sort
+        </button>
+      </div>
+      <div className="w-full bg-green-400" style={{ height: '92%' }}>
+        <div className="flex flex-row justify-evenly" >
+          {array.map((number) => 
+            <div className="bg-red-400" style={{ height: `${number/3}vh`, width: `${1000/size}px` }} >
+            </div> 
+          )}
+        </div>
+      </div>
     </div>
   );
 }
