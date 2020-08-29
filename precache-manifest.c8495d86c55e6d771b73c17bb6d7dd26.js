1691274799152dcfe7ee80431dde8f6f94e59fee
gabrielgeorgiev1998 <gabrielgeorgiev11@gmail.com>
2020-08-29 15:14:42 +0300

    Updates

---
+++ precache-manifest.c8495d86c55e6d771b73c17bb6d7dd26.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112da29670b090d3d10001859555d7a8",
+    "url": "/simple-sort-visualizer/index.html"
+  },
+  {
+    "revision": "b3cd26decbde6351ed39",
+    "url": "/simple-sort-visualizer/static/css/main.edf29e5b.chunk.css"
+  },
+  {
+    "revision": "82210c735e9f86de3a9a",
+    "url": "/simple-sort-visualizer/static/js/2.637f169e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-sort-visualizer/static/js/2.637f169e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cd26decbde6351ed39",
+    "url": "/simple-sort-visualizer/static/js/main.e96a20b3.chunk.js"
+  },
+  {
+    "revision": "8c53910cfbecb6893a43",
+    "url": "/simple-sort-visualizer/static/js/runtime-main.8ba21297.js"
+  },
+  {
+    "revision": "8797e4764829b033cc39f9416fbc917c",
+    "url": "/simple-sort-visualizer/static/media/genNewArrIcon.8797e476.svg"
+  },
+  {
+    "revision": "873ca3445e0378f60a95f8959a949f85",
+    "url": "/simple-sort-visualizer/static/media/sortAlgMenu.873ca344.svg"
+  },
+  {
+    "revision": "fc574009922276ced0fe453715fb424e",
+    "url": "/simple-sort-visualizer/static/media/sortIcon.fc574009.svg"
+  }
+]);
